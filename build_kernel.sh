160f8d4aa9b62a442cbf6e00fbb58a4f9621c6fc
Leah <25106569+RealFX-Code@users.noreply.github.com>
2026-01-10 23:16:13 +0100

    Import N770F/DS Source

---
+++ build_kernel.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#export PATH="/home/leah/toolchains/neutron-clang/bin:$PATH"
+export PATH="/home/leah/aosp1/r7-porting/prebuilts/clang/kernel/linux-x86/clang-r416183b/bin:$PATH"
+
+make \
+    ARCH=arm64 \
+    CC="clang" \
+    LD="ld.lld" \
+    CXX="clang++" \
+    AR="llvm-ar" \
+    AS="llvm-as" \
+    NM="llvm-nm" \
+    STRIP="llvm-strip" \
+    OBJCOPY="llvm-objcopy" \
+    OBJDUMP="llvm-objdump"\
+    OBJSIZE="llvm-size" \
+    READELF="llvm-readelf" \
+    HOSTCC="clang" \
+    HOSTCXX="clang++" \
+    HOSTAR="llvm-ar" \
+    HOSTAS="llvm-as" \
+    HOSTNM="llvm-nm" \
+    HOSTLD="ld.lld" \
+    CROSS_COMPILE=aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi- $@
